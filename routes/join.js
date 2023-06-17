d68417b26d897116fcacc7f3b77350cb596e2608
Burak Coskun <92813347+retr0senss@users.noreply.github.com>
2023-06-17 18:30:23 +0300

    changes

--- routes/join.js
+++ routes/join.js
@@ -1,65 +1,73 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const JoinRequest = require('../schemas/join.request.schema');
-const Event = require('../schemas/event.schema');
+const JoinRequest = require("../schemas/join.request.schema");
+const Event = require("../schemas/event.schema");
 
 // Etkinliklere katılma talebi gönderme
-router.post('/', async (req, res) => {
-    try {
-        const { userId, eventId, requestUserName } = req.body;
-        console.log(userId, eventId);
-        console.log(req.body)
+router.post("/", async (req, res) => {
+  try {
+    const { userId, eventId, requestUserName, ownerId } = req.body;
+    console.log(userId, eventId);
+    console.log(req.body);
 
-        // Kullanıcının var olan bir talebi olup olmadığını kontrol etmek için JoinRequest modelini sorgulayın
-        const existingRequest = await JoinRequest.findOne({ user: userId, event: eventId });
+    // Kullanıcının var olan bir talebi olup olmadığını kontrol etmek için JoinRequest modelini sorgulayın
+    const existingRequest = await JoinRequest.findOne({
+      user: userId,
+      event: eventId,
+    });
 
-        if (existingRequest) {
-            return res.status(400).json({ message: 'You have already sent a join request for this event.' });
-        }
-
-        // Yeni JoinRequest nesnesi oluşturun
-        const joinRequest = new JoinRequest({ user: userId, event: eventId, requestUserName: requestUserName });
-
-        // JoinRequest'i kaydedin
-        await joinRequest.save();
-
-        res.json({ message: 'Join request sent successfully.' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server Error');
+    if (existingRequest) {
+      return res.status(400).json({
+        message: "You have already sent a join request for this event.",
+      });
     }
+
+    // Yeni JoinRequest nesnesi oluşturun
+    const joinRequest = new JoinRequest({
+      user: userId,
+      event: eventId,
+      requestUserName: requestUserName,
+      eventOwner: ownerId,
+    });
+
+    // JoinRequest'i kaydedin
+    await joinRequest.save();
+
+    res.json({ message: "Join request sent successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 });
 
-router.get('/', async (req, res) => {
-    try {
-        const joinRequests = await JoinRequest.find();
-        res.json(joinRequests);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
-    }
+router.get("/", async (req, res) => {
+  try {
+    const joinRequests = await JoinRequest.find();
+    res.json(joinRequests);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // Etkinliklerin statüsünü değiştirme
 
-router.put('/:id', async (req, res) => {
-    try {
-        const joinRequest = await JoinRequest.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    const joinRequest = await JoinRequest.findById(req.params.id);
 
-        if (!joinRequest) {
-            return res.status(404).json({ message: 'Join request not found.' });
-        }
-
-        if (req.body.status) joinRequest.status = req.body.status;
-
-        await joinRequest.save();
-
-        res.json({ message: 'Join request updated successfully.' });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!joinRequest) {
+      return res.status(404).json({ message: "Join request not found." });
     }
-}
-);
+
+    if (req.body.status) joinRequest.status = req.body.status;
+
+    await joinRequest.save();
+
+    res.json({ message: "Join request updated successfully." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
--- schemas/join.request.schema.js
+++ schemas/join.request.schema.js
@@ -1,29 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const JoinRequestSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    event: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Event',
-        required: true
-    },
-    requestUserName: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['Pending', 'Accepted', 'Rejected'],
-        default: 'Pending'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  requestUserName: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Accepted", "Rejected"],
+    default: "Pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  eventOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('JoinRequest', JoinRequestSchema);
+module.exports = mongoose.model("JoinRequest", JoinRequestSchema);
